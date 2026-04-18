def31e7b1309603fb34702eeae92fb9d1c0bcff4
Hagaz <Taiwo.sadiq@decagon.dev>
2026-04-18 23:10:29 +0100

    updated appsettings

---
+++ GHI_ASSET_CARGO.Data/Migrations/20260418213322_AddedHasFinancialFlagToShipment.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GHI_ASSET_CARGO.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedHasFinancialFlagToShipment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Financials_ShipmentId",
+                table: "Financials");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasFinancial",
+                table: "Shipments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Financials_ShipmentId",
+                table: "Financials",
+                column: "ShipmentId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Financials_ShipmentId",
+                table: "Financials");
+
+            migrationBuilder.DropColumn(
+                name: "HasFinancial",
+                table: "Shipments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Financials_ShipmentId",
+                table: "Financials",
+                column: "ShipmentId");
+        }
+    }
+}
